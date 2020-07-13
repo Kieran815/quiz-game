3ec36b62e0bf52a8b51bc916f610586737347946
Kieran815 <kieran.milligan@gmail.com>
2020-07-13 12:36:53 -0500

    beginning styled components

---
+++ src/API.ts
@@ -0,0 +1,35 @@
+// https://opentdb.com/api.php?amount=10&category=29
+import { shuffleArray } from './utils';
+
+// establish `type` for all used variables so JS knows what data types to look for/interpret (i.e. string, number, array, boolean, any, null, etc)
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+//
+export type QuestionState = Question & { answers: string[] }
+
+// use `enum` to 
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard'
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=29&difficulty=${difficulty}`;
+  const data = await (await fetch(endpoint)).json();
+
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer
+    ]) 
+  }));
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { fetchQuizQuestions, QuestionState, Difficulty } from './API';
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
+
+// easy to modify
+const TOTAL_QUESTIONS = 10;
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  // import `types` for state
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number , setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] =  useState(true);
+  
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM));
+  //
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    // pass values to function from `./API`;
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.MEDIUM
+    );
+
+    // resets game with 0 points, blank arrays, and new questions
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  }
+
+  //
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver) {
+      // save user answer
+      const answer = e.currentTarget.value;
+      // compare to correct answer
+      const correct = questions[number].correct_answer === answer;
+      if(correct) {
+        setScore(score + 1)
+      }
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
+  }
+
+  //
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Quiz</h1>
+      {/* Ternary Op to conditionally render `Start` button, `Score`, and `QuestionCard` */}
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ?
+        <button onClick={startQuiz}>Start</button> : null
+      }
+      {!gameOver ? <p className='score'>Score: {score}</p> : ''}
+      {loading ? <p>Loading Quiz</p> : ''}
+      {!loading && !gameOver ?
+        <QuestionCard
+          questionNumber={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        /> :
+        null
+      }
+
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ?
+        <button
+          className="next"
+          onClick={nextQuestion}
         >
-          Learn React
-        </a>
-      </header>
+          Next Question
+        </button> :
+        ''
+      }
     </div>
   );
 }
 
 export default App;
+
