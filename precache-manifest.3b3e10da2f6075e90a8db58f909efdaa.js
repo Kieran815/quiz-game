b335c6b8e8db6c83741ef33a6404792917a68a86
Kieran815 <kieran.milligan@gmail.com>
2020-09-06 19:52:43 -0500

    Updates

---
+++ precache-manifest.3b3e10da2f6075e90a8db58f909efdaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0472d65aaf708000dd8f28726348923",
+    "url": "/quiz-game/index.html"
+  },
+  {
+    "revision": "73b590c312ee38c31638",
+    "url": "/quiz-game/static/js/2.5a5027be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-game/static/js/2.5a5027be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8b9942709bf68c34fd",
+    "url": "/quiz-game/static/js/main.1946f250.chunk.js"
+  },
+  {
+    "revision": "8bccf2c5809be0afac0a",
+    "url": "/quiz-game/static/js/runtime-main.4057fbda.js"
+  },
+  {
+    "revision": "3693f61ddac670c0111424e82dbfa519",
+    "url": "/quiz-game/static/media/marvelVdc.3693f61d.jpg"
+  }
+]);
